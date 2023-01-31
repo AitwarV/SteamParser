a982605fec32ed8f199401db3f8bdeb26e6b07f1
Dinislam Gilyazov <den.gilyazov@yandex.ru>
2023-01-31 17:49:32 +0500

    init: miniUI

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 import json
 import time
 from selenium.webdriver.common.by import By
@@ -5,33 +6,58 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-URL_TEMPLATE = "https://steamcommunity.com/market/listings/730/StatTrak™%20AWP%20%7C%20Asiimov%20%28Battle-Scarred%29"
 
-my_service = Service('C:\WebDriver_Chrome\chromedriver.exe')
-my_options = Options()
-my_options.add_extension("D:\WebDriver_Chrome\extension\extension_1_18_12_0.crx")
-my_options.add_argument("--disable-infobars")
-my_options.add_argument("start-maximized")
-browser = webdriver.Chrome(service=my_service, options=my_options)
+root = Tk()
 
-browser.get(URL_TEMPLATE)
+def btn_click():
+    URL_TEMPLATE = urlInput.get()
 
-label = browser.find_element(By.XPATH, "//label[text()='Get skin data, by default']")
-checkbox = label.find_element(By.CSS_SELECTOR, 'span.ui-checkboxradio-icon.ui-corner-all.ui-icon.ui-icon-background.ui-icon-blank').click()
+    my_service = Service('C:\webdriver\chromedriver.exe')
+    my_options = Options()
+    my_options.add_extension("C:\webdriver\i1.18.13_0.crx")
+    my_options.add_argument("--disable-infobars")
+    my_options.add_argument("start-maximized")
+    browser = webdriver.Chrome(service=my_service, options=my_options)
 
-items = browser.find_elements(By.CLASS_NAME, 'market_listing_item_name_block')
-floats = browser.find_elements(By.CLASS_NAME, 'itemfloat')
-time.sleep(30)
-name = [x.text for x in items]
-floats_item = [x.text for x in floats]
+    browser.get(URL_TEMPLATE)
 
-result_json = []
-for floats_item, name in zip(floats_item, name):
-    result_json.append({
-        'name': name,
-        'float': floats_item
-    })
+    label = browser.find_element(By.XPATH, "//label[text()='Get skin data, by default']")
+    checkbox = label.find_element(By.CSS_SELECTOR, 'span.ui-checkboxradio-icon.ui-corner-all.ui-icon.ui-icon-background.ui-icon-blank').click()
 
-with open('res.json', 'w', encoding='utf-8') as file:
-    json.dump(result_json, file, indent=4, ensure_ascii=False)
+    items = browser.find_elements(By.CLASS_NAME, 'market_listing_item_name_block')
+    floats = browser.find_elements(By.CLASS_NAME, 'itemfloat')
+    time.sleep(1)
+    name = [x.text for x in items]
+    floats_item = [x.text for x in floats]
+
+    result_json = []
+    for floats_item, name in zip(floats_item, name):
+        result_json.append({
+            'name': name,
+            'float': floats_item
+        })
+
+    with open('res.json', 'w', encoding='utf-8') as file:
+        json.dump(result_json, file, indent=4, ensure_ascii=False)
+
+root.title('SteamParser')
+root['bg'] = '#000000'
+root.geometry('300x250')
+root.resizable(width=False, height=False)
+
+canvas = Canvas(root, height=600, width=600)
+canvas.pack()
+
+frame = Frame(root, bg = '#696969')
+frame.place(relwidth=1,relheight=1)
+
+urlInput = Entry(frame, bg = 'white')
+urlInput.pack()
+
+btn = Button(frame, text = 'parse', bg = 'white', command=btn_click)
+btn.pack()
+
+root.mainloop()
+
+
 
